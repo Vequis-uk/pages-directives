0b3a515121e9ca58bb69080e54309045208a8595
David Bonser <davidbonzer@hotmail.co.uk>
2022-09-02 12:27:56 +0100

    added ngswitch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -90,4 +90,9 @@ export class AppComponent {
       url: 'https://images.unsplash.com/photo-1661922031222-7f316ee30ff6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
     },
   ];
+
+  checkWindowIndex(index: number){
+    return Math.abs(this.currentPage - index) < 5;
+  }
+
 }
